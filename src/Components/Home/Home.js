d40c584b47d0903baa59aaa9085f49c1aa1679ba
VINAYAK V. UTEKAR <93907309+VinayakUtekar@users.noreply.github.com>
2023-08-05 11:53:52 +0530

    Final design of Home.js

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,30 +1,41 @@
-import React from "react";
+import React,{useEffect} from "react";
 import "./Home.css";
 import video from '../../Media/Background-video.mp4'
 import {GrLocation} from "react-icons/gr"
-
+import {HiFilter} from "react-icons/hi"
+import {FiFacebook} from "react-icons/fi";
+import {AiOutlineInstagram} from "react-icons/ai";
+import {AiFillYoutube} from "react-icons/ai";
+import {BsListTask} from "react-icons/bs";
+import {TbApps} from "react-icons/tb";
+import Aos from 'aos';
+import 'aos/dist/aos.css'
 
 const Home = () =>{
+    useEffect(() => {
+        Aos.init({duration: 2000})
+    }, [])
+
     return(
         <section className="home">
-            <div className="overlay"></div>
-            <video src={video} muted autoPlay loop type="video/mp4"></video>
-        
+            <div className="videoDiv"> 
+                <video src={video} muted autoPlay loop type="video/mp4"></video>
+            </div>
+            
             <div className="homeContent container">
                 <div className="textDiv">
-
-                    <span className="smallText">
+                    <span data-aos="fade-up" className="smallText">
                         Our Packages
                     </span>
 
-                    <h1 className="homeTitle">
+                    <h1 data-aos="fade-up" className="homeTitle">
                         Search your Holiday
                         </h1>
                 </div>
 
-                <div className="cardDiv grid">
+                <div data-aos="fade-up" className="cardDiv grid">
                     <div className="destinationInput">
-                        <label htmlFor="city">
+                        <label className="label" htmlFor="city">
                             Search your Destination:
                         </label>
                         <div className="input flex">
@@ -33,7 +44,7 @@ const Home = () =>{
                         </div>
                     </div>
                     <div className="dateInput">
-                        <label htmlFor="date">
+                        <label className="label" htmlFor="date">
                             Select your date:
                         </label>
                         <div className="input flex">
@@ -42,14 +53,31 @@ const Home = () =>{
                     </div>
                     <div className="priceInput">
                         <div className="label_total flex">
-                            <label htmlFor="price">
+                            <label className="label" htmlFor="price">
                                 Max price
                             </label>
                             <h3 className="total">$5000</h3>
                         </div>
                         <div className="input flex">
-                            <input type="range" max="5000" min="1000"/>                           
+                            <input type="range" max="5000" min="1000" value="3500"/>                           
                         </div>
+                    </div>
+
+                    <div className="searchOptions flex">
+                        <HiFilter className="icon"/>
+                        <span>MORE FILTERS</span>
+                    </div>
+                </div>
+
+                <div data-aos="fade-up" className="homeFooterIcons flex">
+                    <div className="rightIcons">
+                        <FiFacebook className="icon"/>
+                        <AiOutlineInstagram className="icon"/>
+                        <AiFillYoutube className="icon"/>
+                    </div>
+                    <div className="lefIcons">
+                        <BsListTask className="icon"/>
+                        <TbApps className="icons"/>
                     </div>
                 </div>
             </div>
